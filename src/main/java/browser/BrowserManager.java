263b9087a0944b5950c73dadfde6f1c0b462429b
raghavendraj <raghavendraj27@gmail.com>
2026-03-03 15:49:32 +0530

    Implementing the Browser Manager

---
+++ src/main/java/browser/BrowserManager.java
@@ -0,0 +1,34 @@
+package browser;
+
+import com.microsoft.playwright.*;
+
+import java.awt.*;
+
+public class BrowserManager {
+
+    public Playwright playwright;
+    public Page page;
+    public Browser browser;
+    public BrowserContext context;
+
+
+    public void setUp(){
+        System.out.println("Playwright setup....");
+        Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) screensize.getWidth();
+        int height = (int) screensize.getHeight();
+
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context = browser.newContext(new Browser.NewContextOptions().setViewportSize(width,height));
+        page = context.newPage();
+    }
+
+
+    public void tearDown(){
+        if(page != null) page.close();
+        if(browser != null) browser.close();
+        if(playwright != null) playwright.close();
+    }
+
+}
